6ed6d9dcea9983f0c22f618e26d5495a3ccb7794
Anathan <anathan.gv@gmail.com>
2020-04-04 18:06:24 -0300

    First basic app developed using react

---
+++ React/robofriends/src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Maria',
+		username: 'mar',
+		email: 'maria@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Pedro',
+		username: 'ped',
+		email: 'pedro@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'João',
+		username: 'joa',
+		email: 'joa@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Joana',
+		username: 'joa',
+		email: 'joa@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Matt',
+		username: 'mat',
+		email: 'mat@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Ana',
+		username: 'ana',
+		email: 'ana@gmail.com'
+	}
+]
